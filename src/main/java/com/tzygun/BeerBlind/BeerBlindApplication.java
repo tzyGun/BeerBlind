0c1a03d20e3a95eaa6b372039998671a08832dc0
tzygun <tzygun>
2022-12-28 08:39:38 +0100

    initial commit

---
+++ src/main/java/com/tzygun/BeerBlind/BeerBlindApplication.java
@@ -0,0 +1,13 @@
+package com.tzygun.BeerBlind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeerBlindApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeerBlindApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tzygun/BeerBlind/BeerBlindApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tzygun.BeerBlind;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeerBlindApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
